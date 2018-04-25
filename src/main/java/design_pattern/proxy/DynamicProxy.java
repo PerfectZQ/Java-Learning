d47c18ca8ffd3486de7ef7fe574000607ca78cec
zhangqiang <inteli_zq@163.com>
2018-04-25 14:23:33 +0800

    modify

--- src/main/java/design_pattern/proxy/DynamicProxy.java
+++ src/main/java/design_pattern/proxy/DynamicProxy.java
@@ -18,30 +18,27 @@ public class DynamicProxy {
         InvocationHandler handler = new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                // 调用委托对象方法
+                Object obj = method.invoke(caseClient, args);
+                // 扩展功能
                 if ("litigation".equals(method.getName())) {
-                    // 调用委托对象方法
-                    caseClient.litigation();
-                    // 扩展功能
                     System.out.println("了解案情");
                     System.out.println("收集证据");
                     System.out.println("辩护");
                     System.out.println("打赢官司");
 
-                } else if("draftingContracts".equals(method.getName())) {
-                    // 调用委托对象方法
-                    caseClient.draftingContracts();
+                } else if ("draftingContracts".equals(method.getName())) {
                     // 扩展功能
                     System.out.println("审核合同");
                     System.out.println("修订合同");
                     System.out.println("保证当事人合法利益");
                 }
-                return null;
+                return obj;
             }
         };
 
-        // 创建代理对象
-        SomethingCantDoByYourself proxy =
-                (SomethingCantDoByYourself) Proxy.newProxyInstance(clientClass.getClassLoader(), clientClass.getInterfaces(), handler);
+        // 根据传递的委托类接口，通过反射创建对应的代理对象
+        SomethingCantDoByYourself proxy = (SomethingCantDoByYourself) Proxy.newProxyInstance(clientClass.getClassLoader(), clientClass.getInterfaces(), handler);
         // 调用代理对象方法
         proxy.litigation();
         System.out.println("================== 笔直的分割线 ====================");
@@ -49,26 +46,4 @@ public class DynamicProxy {
     }
 }
 
-
-
-class LawyerHandler implements InvocationHandler {
-
-    private Object client;
-
-    public LawyerHandler(Object client) {
-        this.client = client;
-    }
-
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        // 调用委托对象方法
-        Object obj = method.invoke(client, args);
-        // 扩展功能
-        System.out.println("了解案情");
-        System.out.println("收集证据");
-        System.out.println("辩护");
-        System.out.println("打赢官司");
-        return obj;
-    }
-}
 
