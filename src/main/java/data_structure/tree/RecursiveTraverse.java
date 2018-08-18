0f7ecfdb358e46e050c8c11e217bd43e78a7fe28
ZhangQiang <inteli_zq@163.com>
2018-08-18 16:22:38 +0800

    init

--- src/main/java/data_structure/tree/RecursiveTraverse.java
+++ src/main/java/data_structure/tree/RecursiveTraverse.java
@@ -1,4 +1,16 @@
 package data_structure.tree;
 
 public class RecursiveTraverse {
+
+    /**
+     * 中序遍历
+     *
+     * @param treeNode
+     */
+    public static void inOrderRecursiveTraverse(TreeNode treeNode) {
+
+        if (treeNode.left != null) inOrderRecursiveTraverse(treeNode.left);
+        System.out.println(treeNode.name);
+        if (treeNode.right != null) inOrderRecursiveTraverse(treeNode.right);
+    }
 }
--- src/main/java/data_structure/tree/StackTraverse.java
+++ src/main/java/data_structure/tree/StackTraverse.java
@@ -1,4 +1,34 @@
 package data_structure.tree;
 
+import java.util.Stack;
+
 public class StackTraverse {
+
+    /**
+     * 中序遍历
+     *
+     * @param treeNode
+     */
+    public static void inOrderTraverse(TreeNode treeNode) {
+        if (treeNode == null) return;
+
+        Stack<TreeNode> s = new Stack<>();
+
+        TreeNode currentNode = treeNode;
+        s.push(currentNode);
+
+        while (!s.empty()) {
+            if (currentNode.left != null) {
+                currentNode = currentNode.left;
+                s.push(currentNode);
+            } else {
+                currentNode = s.pop();
+                System.out.println(currentNode.name);
+                if (currentNode.right != null) {
+                    currentNode = currentNode.right;
+                    s.push(currentNode);
+                }
+            }
+        }
+    }
 }
