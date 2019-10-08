a38dbb57bf2e5de596d35956615311d3572b22f8
张强 <inteli_zq@163.com>
2019-10-08 19:55:41 +0800

    Update HeapSort.java

--- src/main/java/algorithms/sort/HeapSort.java
+++ src/main/java/algorithms/sort/HeapSort.java
@@ -22,7 +22,9 @@ public class HeapSort {
             if (leftNode + 1 <= end && seq[leftNode].compareTo(seq[leftNode + 1]) < 0) leftNode++;
             // 如果父节点小于子节点中最大的节点，则进行交换
             if (seq[start].compareTo(seq[leftNode]) < 0) {
+                // 原始节点和最大节点位置进行交换
                 swap(seq, start, leftNode);
+                // 将开始指针恢复到被交换后的原始节点位置，继续调整子堆
                 start = leftNode;
             } else return;
         }
@@ -45,11 +47,17 @@ public class HeapSort {
 
 
     public <T extends Comparable<? super T>> void heapSort(T[] a) {
-        // 初始化最大堆，从第一个有子节点的节点开始从下往上调整
+        // 初始化最大堆，从第一个有子节点的节点(a.length / 2 - 1)开始从下往上调整
+        //              0
+        //        1            2
+        //    3       ④     5      6
+        //  7   8   9
+        // 例如 length=10，则下标为 10/2-1=4 的节点即为第一个有子节点的节点，调整完所有子树后将最大元素移到根节点(下标为0)
         for (int firstNode = a.length / 2 - 1; firstNode >= 0; firstNode--) adjustment(a, firstNode, a.length - 1);
         // 排序
         for (int index = a.length - 1; index > 0; index--) {
             swap(a, 0, index);
+            // 每次只调整根节点的树就可以了
             adjustment(a, 0, index - 1);
         }
     }
